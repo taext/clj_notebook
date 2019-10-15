2a82cadbb44d2c92ea2fca6152ade60ad3b7eafc
David <gh@v1d.dk>
2019-10-15 23:27:27 +0200

    initial commit

---
+++ log.clj
@@ -0,0 +1,3 @@
+
+#notebook.Note{:body "I'm a real Lisp (Clojure) programmer today (!) :D", :tags ["dagbog" "oktober"], :date-created "2019-10-15", :time-created "23:25:48", :location "Lejligheden, DK"}
+
---
+++ notebook7.clj
@@ -0,0 +1,23 @@
+(ns notebook)
+(require '[clojure.string :as str])
+
+(defrecord Note [body tags date-created time-created location])
+
+(def date-now (get (str/split (str (java.time.LocalDateTime/now)) #"T") 0))
+
+(def time-of-day (get (str/split (str (java.time.LocalDateTime/now)) #"T") 1))
+
+(def time-now (get (str/split time-of-day #"\.") 0))  ; remove milliseconds
+
+(defn write-to-log [my-note] (spit "log.clj" (str (pr-str my-note) "\n\n") :append true))
+
+(defn build-note [body-text tags location]
+  (def this-note (->Note body-text tags (str date-now) (str time-now) location))
+  (write-to-log this-note)
+  (prn-str this-note)
+  )
+
+; (def my-note (build-note "Today cool things happened..." ["dagbog" "oktober"] "Lejligheden, DK"))
+
+
+
