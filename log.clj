733d8d7534685947f85308ad7ce79598c2a28c36
David <gh@v1d.dk>
2019-10-15 23:55:59 +0200

    u

--- log.clj
+++ log.clj
@@ -1,5 +1,3 @@
 
-#notebook.Note{:body "I'm a real Lisp (Clojure) programmer today (!) :D", :tags ["dagbog" "oktober"], :date-created "2019-10-15", :time-created "23:45:35", :location "Lejligheden, DK"}
-
 #notebook.Note{:body "I'm a real Lisp (Clojure) programmer today (!) :D", :tags ["dagbog" "oktober"], :date-created "2019-10-15", :time-created "23:52:47", :location "Lejligheden, DK"}
 
