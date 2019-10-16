ff7ac0c144988472caff43540bd9338643ac4ff1
David <gh@v1d.dk>
2019-10-16 15:17:32 +0200

    u

--- log.clj
+++ log.clj
@@ -1,3 +1,9 @@
 
 #notebook.Note{:body "I'm a real Lisp (Clojure) programmer today (!) :D", :tags ["dagbog" "oktober"], :date-created "2019-10-15", :time-created "23:52:47", :location "Lejligheden, DK"}
 
+#notebook.Note{:body "Testing CLI note input...", :tags ("test" "dagbog"), :date-created "2019-10-16", :time-created "14:57:30", :location "Lejligheden, DK"}
+
+#notebook.Note{:body "And we have functional CLI notebook running...!", :tags ("testing" "clj_notebook" "dagbog" "succes-oplevelse"), :date-created "2019-10-16", :time-created "15:07:33", :location "Lejligheden, DK"}
+
+#notebook.Note{:body "Calling note script for ~/Documents, totally works", :tags ("testing"), :date-created "2019-10-16", :time-created "15:15:04", :location "Lejligheden, DK"}
+
