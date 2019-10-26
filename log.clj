357af2bb5b30ef466c9d91f2b89e4da99f3b2f4e
David <gh@v1d.dk>
2019-10-26 10:27:26 +0200

    updated

--- log.clj
+++ log.clj
@@ -5,5 +5,23 @@
 
 #notebook.Note{:body "And we have functional CLI notebook running...!", :tags ("testing" "clj_notebook" "dagbog" "succes-oplevelse"), :date-created "2019-10-16", :time-created "15:07:33", :location "Lejligheden, DK"}
 
-#notebook.Note{:body "Calling note script for ~/Documents, totally works", :tags ("testing"), :date-created "2019-10-16", :time-created "15:15:04", :location "Lejligheden, DK"}
+#notebook.Note{:body "Calling note script for ~/Documents, totally works", :tags ("testing"), :date-created "2019-10-16", :time-created "15:23:02", :location "Lejligheden, DK"}
+
+#notebook.Note{:body "Fixing output nil bug (by using str)", :tags ("testing" "clj_notebook"), :date-created "2019-10-16", :time-created "15:24:48", :location "Lejligheden, DK"}
+
+#notebook.Note{:body "Shortening Lejligheden, DK location to LDK", :tags ("testing" "clj_notebook"), :date-created "2019-10-16", :time-created "15:25:51", :location "LDK"}
+
+#notebook.Note{:body "Found out CLI quotes are only needed with white-space - sweet...", :tags ("testing" "clj_notebook"), :date-created "2019-10-16", :time-created "15:27:40", :location "LDK"}
+
+#notebook.Note{:body "OMG, so excited about Clojure...!", :tags ("dagbog" "clojure"), :date-created "2019-10-16", :time-created "18:02:10", :location "LDK"}
+
+#notebook.Note{:body "Watching Security Now (https://www.youtube.com/watch?v=dYSlTIqyn3I), great stuff", :tags ("security" "podcast"), :date-created "2019-10-16", :time-created "18:06:17", :location "LDK"}
+
+#notebook.Note{:body "Calling clj_notebook from Python using sh #omg", :tags ("testing" "clj_notebook"), :date-created "2019-10-16", :time-created "18:08:35", :location "LDK"}
+
+#notebook.Note{:body "Fixed vsub_paste .json download bug (by updating youtube-dl - mental note: code a youtube-dl .json download test)", :tags ("dagbog" "linux"), :date-created "2019-10-16", :time-created "18:33:27", :location "LDK"}
+
+#notebook.Note{:body "https://hackernoon.com/5-ways-open-finance-is-changing-money-forever-28o44z2", :tags ("muligt_blogmateriale" "cryptocurrency"), :date-created "2019-10-25", :time-created "18:04:17", :location "LDK"}
+
+#notebook.Note{:body "Online teaching platform https://www.thinkific.com/", :tags ("teaching" "tools"), :date-created "2019-10-25", :time-created "18:13:22", :location "LDK"}
 
